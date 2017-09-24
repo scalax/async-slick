1a79883b7df9f8d9d7c5678579f7f3e66b59da7b
djx314 <djx314@sina.cn>
2017-09-24 22:08:27 +0800

    Add slick source.

---
+++ src/main/scala/async/slick/jdbc/meta/MSuperTable.scala
@@ -0,0 +1,16 @@
+package slick.async.jdbc.meta
+
+import slick.jdbc.ResultSetAction
+
+/** A wrapper for a row in the ResultSet returned by DatabaseMetaData.getSuperTables(). */
+case class MSuperTable(table: MQName, superTable: String) {
+  def getSuperTables = MSuperTable.getSuperTables(MQName(table.catalog, table.schema, superTable))
+}
+
+object MSuperTable {
+  def getSuperTables(tablePattern: MQName) = ResultSetAction[MSuperTable](
+    _.metaData.getSuperTables(tablePattern.catalog_?, tablePattern.schema_?, tablePattern.name)
+  ) { r =>
+      MSuperTable(MQName.from(r), r.<<)
+    }
+}
---
+++ src/main/scala/async/slick/jdbc/package.scala
@@ -0,0 +1,6 @@
+package async.slick
+/**
+ * Contains the abstract `JdbcProfile` and related code. This includes all JDBC-related code,
+ * facilities for <em>Plain SQL</em> queries, and JDBC-specific profile components.
+ */
+package object jdbc
